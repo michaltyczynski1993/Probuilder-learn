fbcef7a757ecf16a35823a9b0880394c9ea98b36
Michał <m.tyczynski1993@gmail.com>
2021-12-11 17:14:47 +0100

    Main Menu Buttons interations
    1. Created Button OnClick() event for Controls Panel.

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject controlPanel;
     public void NewGame()
     {
         SceneManager.LoadScene("Level1");
     }
     public void Credits(){}
-    public void Controls(){}
+    public void ControlsShowAndHide()
+    {
+        if(controlPanel.active == false)
+        {
+            controlPanel.SetActive(true);
+        }
+        else 
+        {
+            controlPanel.SetActive(false);
+        }
+    }
     public void GameExit()
     {
         Application.Quit();
