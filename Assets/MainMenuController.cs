d28a08d110b6c3cc492744647c99351fea371335
Michał <m.tyczynski1993@gmail.com>
2021-12-13 07:07:16 +0100

    Main Menu Gamepad support implementation
    Script for first delected gameobjects in every situation (eg. controls menu open/closed, when game start)

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject controlPanel;
+    public GameObject controlsFirstSelect, controlsFirstClosed;
+    
     public void NewGame()
     {
         SceneManager.LoadScene("Level1");
     }
     public void Credits(){}
-    public void ControlsShowAndHide()
+    public void ControlsOpen()
     {
-        if(controlPanel.active == false)
-        {
-            controlPanel.SetActive(true);
-        }
-        else 
-        {
-            controlPanel.SetActive(false);
-        }
+        controlPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(controlsFirstSelect);
+    }
+    public void ControlsClosed()
+    {
+        controlPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(controlsFirstClosed);
     }
     public void GameExit()
     {
